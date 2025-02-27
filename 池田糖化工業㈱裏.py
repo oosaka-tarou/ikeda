5d9f4cda925285fc895a0e1da8ba1a72b0da3dab
oosaka-tarou <lee96331@yahoo.co.jp>
2025-02-27 18:18:22 +0900

    first commit

---
+++ 池田糖化工業㈱裏.py
@@ -0,0 +1,31 @@
+import streamlit as st
+
+
+st.title("池田糖化工業㈱裏サイト")
+
+
+st.markdown("""
+## 概要
+このページは、以下の構成になっています。
+
+
+### 構成
+１. 会社紹介
+
+２. 箕島工場屋外の風景
+
+３. 箕島工場屋内の風景
+
+４. 部署紹介
+
+５. 利用権入力
+
+６. 管理者使用
+
+７. 池田糖化工業㈱裏リンクサイト集
+
+""")
+
+#st.markdown("---") #区切り線
+#st.link_button("池田糖化工業㈱裏サイトリンク集", "https://ikedaura.streamlit.app/",use_container_width=True,type="primary" )
+#st.markdown("---") #区切り線
